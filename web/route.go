d4aab65231888203afe97a8dbce572ba840a5a8b
jiarui <sonven@jiaruideMacBook-Pro.local>
2015-04-27 00:14:48 +0800

    route refactor

--- web/route.go
+++ web/route.go
@@ -7,39 +7,36 @@ import (
 
 //路由映射
 type RouteMap struct {
-	deferFunc http.HandlerFunc
+	deferFunc HttpContextFunc
 	//地址模式
 	UrlPatterns []string
 	//路由集合
-	RouteCollection map[string]func(http.ResponseWriter, *http.Request)
+	RouteCollection map[string]HttpContextFunc
 }
 
 // HTTP处理词典
-type httpFuncMap map[string]func(http.ResponseWriter, *http.Request)
+type httpFuncMap map[string]HttpContextFunc
 
 //添加路由
-func (this *RouteMap) Add(
-	urlPattern string,
-	requestFunc func(http.ResponseWriter, *http.Request)) {
+func (this *RouteMap) Add(urlPattern string,rf HttpContextFunc) {
 	if this.RouteCollection == nil {
-		this.RouteCollection =
-			make(map[string]func(http.ResponseWriter, *http.Request))
+		this.RouteCollection = make(map[string]HttpContextFunc)
 	}
 	_, exists := this.RouteCollection[urlPattern]
 
 	if !exists {
-		this.RouteCollection[urlPattern] = requestFunc
+		this.RouteCollection[urlPattern] = rf
 		this.UrlPatterns = append(this.UrlPatterns, urlPattern)
 	}
 }
 
 // 处理请求
-func (this *RouteMap) Handle(w http.ResponseWriter, r *http.Request) {
+func (this *RouteMap) Handle(ctx *Context) {
 	// 执行某些操作，如捕获异常等
 	if this.deferFunc != nil {
-		defer this.deferFunc(w, r)
+		defer this.deferFunc(ctx)
 	}
-
+	r,w := ctx.Request,ctx.ResponseWriter
 	routes := this.RouteCollection
 	path := r.URL.Path
 	var isHandled bool = false
@@ -51,57 +48,23 @@ func (this *RouteMap) Handle(w http.ResponseWriter, r *http.Request) {
 			matched, err := regexp.Match(k, []byte(path))
 			//fmt.Println("\n",k,path)
 			if err != nil {
-				w.Write([]byte(err.Error()))
+				http.Error(w,err.Error(),http.StatusInternalServerError)
 				return
 			}
 			if matched && v != nil {
 				isHandled = true
-				v(w, r)
+				v(ctx)
 				break
 			}
 		}
 	}
 
 	if !isHandled {
-		w.Write([]byte("404 Not found!"))
+		http.Error(w,"404 Not found!",http.StatusNotFound)
 	}
 }
 
 // 延迟执行的操作，发生在请求完成后
-func (this *RouteMap) Defer(f http.HandlerFunc) {
+func (this *RouteMap) Defer(f HttpContextFunc) {
 	this.deferFunc = f
-}
-
-//处理路由请求
-//todo: will be remove.
-func handleMapRoute(w http.ResponseWriter, r *http.Request, routes httpFuncMap) {
-	path := r.URL.Path
-	var isHandled bool = false
-
-	for k, v := range routes {
-		if !isHandled {
-			matched, err := regexp.Match(k, []byte(path))
-
-			//			if path == "/a/" {
-			//				fmt.Println(k + "==>" + path)
-			//				fmt.Println(matched)
-			//				fmt.Println(v)
-			//			}
-
-			if err != nil {
-				w.Write([]byte(err.Error()))
-				return
-			}
-			if matched && v != nil {
-				isHandled = true
-				v(w, r)
-				//fmt.Println("----")
-				break
-			}
-		}
-	}
-
-	if !isHandled {
-		w.Write([]byte("404 Not found!"))
-	}
 }
