b3a6bea7c7ca63d8d09cb9586fb180eeb7a447e8
= <=>
2015-04-29 22:36:26 +0800

    redis_storage

--- storage/redis_storage.go
+++ storage/redis_storage.go
@@ -11,50 +11,63 @@ package storage
 import (
 	"github.com/atnet/gof"
 	"github.com/garyburd/redigo/redis"
-	"fmt"
 	"bytes"
 	"encoding/gob"
+    "sync"
 )
 
 type redisStorage struct {
 	_pool *redis.Pool
-	_buf bytes.Buffer
+	_buf *bytes.Buffer
+    sync.Mutex
 }
 
 func NewRedisStorage(pool *redis.Pool) gof.Storage {
 	return &redisStorage{
 		_pool: pool,
+        _buf : new(bytes.Buffer),
 	}
 }
 
 func (this *redisStorage) Get(key string, dst interface{}) error {
 	src, err := redis.Bytes(this._pool.Get().Do("Get", key))
 	if err == nil{
-		buf :=
-		dec := gob.Decoder(buf)
-		buf.Reset()
+        err = this.decodeBytes(src,dst)
 	}
 	return err
 }
 
-func getByte(v interface{})([]byte,error){
-	buf := new(bytes.Buffer)
-	enc := gob.NewEncoder(buf)
+func (this *redisStorage) getByte(v interface{})([]byte,error){
+    this.Mutex.Lock()
+    defer this.Mutex.Unlock()
+	enc := gob.NewEncoder(this._buf)
 	err := enc.Encode(v)
 	if err == nil {
 		return nil,err
 	}
-	return buf.Bytes(),err
+    b := this._buf.Bytes()
+    this._buf.Reset()
+	return b,err
 }
+
+func (this *redisStorage) decodeBytes(b []byte,dst interface{})error {
+    this.Mutex.Lock()
+    defer this.Mutex.Unlock()
+    dec := gob.NewDecoder(this._buf)
+    err := dec.Decode(dst)
+    this._buf.Reset()
+    return err
+}
+
 func (this *redisStorage) Set(key string, v interface{})error {
-	b,err := getByte(v)
+	b,err := this.getByte(v)
 	if err == nil {
 		_, err = this._pool.Get().Do("SET", key, b)
 	}
 	return err
 }
 func (this *redisStorage) SetExpire(key string, v interface{}, seconds int32)error {
-	b,err := getByte(v)
+	b,err := this.getByte(v)
 	if err == nil {
 		_, err = this._pool.Get().Do("SETEX", key, b,seconds)
 	}
