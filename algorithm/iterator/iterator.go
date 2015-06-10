daad10f4e92349777ec2d8a0816326c660423f06
jarryliu <jarrysix@gmail.com>
2015-06-10 19:58:27 +0800

    iterator

---
+++ algorithm/iterator/iterator.go
@@ -0,0 +1,29 @@
+/**
+ * Copyright 2015 @ S1N1 Team.
+ * name : iterator
+ * author : jarryliu
+ * date : -- :
+ * description : 迭代器
+ * history :
+ */
+package iterator
+
+// 处理单个对象
+type WalkFunc func(v interface{}, level int)
+
+// 迭代时满足的条件
+type Condition func(v,v1 interface{})bool
+
+// 迭代栏目,start为开始前执行函数,over为结束迭代执行函数
+func Walk(categories []interface{}, v interface{},c Condition,
+    start WalkFunc,over WalkFunc, level int) {
+    start(v, level)
+    for _, v1 := range categories {
+        if c(v, v1) {
+            Walk(categories, v1, c, start, over, level+1)
+        }
+    }
+    if over != nil {
+        over(v,level)
+    }
+}
