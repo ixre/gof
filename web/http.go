425b93bd20ac2f90330a55867a957253f1fec884
jarryliu <jarrysix@gmail.com>
2015-09-21 19:36:48 +0800

    commit

--- web/http.go
+++ web/http.go
@@ -20,38 +20,35 @@ func HttpError(rsp http.ResponseWriter, err error) {
 	rsp.Header().Add("Content-Type", "text/html")
 	rsp.WriteHeader(500)
 
-	var part1 string = `<html><head><title>HTTP ERROR </title>
+	var part1 string = `<html><head><title>HTTP ERROR</title>
 				<meta charset="utf-8"/>
 				<style>
-				body{background:#FFF;font-size:100%;color:#333;margin:0 2%;}
-        h1{color:red;font-size:28px;border-bottom:solid 1px #ddd;line-height:80px;}
-        div.except-panel p{margin:20px 0;}
-        div.except-panel div.summary{}
-        div.except-panel p.message{font-size:24px;}
-        div.except-panel p.contact{color:#666;font-size:18px;}
-        div.except-panel p.stack{padding-top:30px;}
-        div.except-panel p.stack em{font-size:18px;font-style: normal;}
-        div.except-panel pre{font-family: Sans,Arail;
-            border:solid 1px #ddd;padding:20px;
-            font-size:16px;background:#F5F5F5;
-            line-height: 150%;color:#888;}
-        div.except-panel .hidden{display:none;}
+	body{background:#FFF;font-size:100%;margin:0 0 2em 0;}
+	div.tit{background:#FFA;border-bottom:solid 1px #FE0;}
+    h1{color:#F00;font-size:2em;line-height:2em;margin:0;border-bottom:solid 1px #FFF;padding:0.8em 2%;}
+    div.except-panel p{margin:0 2%;padding:0}
+    div.except-panel div.summary{color:#000;}
+    div.except-panel p.message{font-size:1.4em;margin:2em 2% 0 2%;}
+    div.except-panel p.contact{color:#666;font-size:1.2em;}
+    div.except-panel p.stack{padding:0;}
+    div.except-panel em{font-size:1.2em;font-style: normal;color:#000;line-height:2em;font-weight:bold;}
+    div.except-panel pre{font-family: Sans,Arail;margin:1em 2% 2em 2%;line-height: 150%;color:#333;}
+    div.except-panel .hidden{display:none;}
 			</style>
         </head>
         <body>`
 
 	var html string = fmt.Sprintf(`
-				<h1>HTTP ERROR ：%s</h1>
+				<div class="tit"><h1>%s</h1></div>
 				<div class="except-panel">
 					<div class="summary">
 						<p class="message">Source：%s&nbsp;&nbsp;Line:%d</p>
-						<p class="contact">Plese contact administrator or <a href="/">go home</a></p>
 					</div>
 					<p class="stack">
-						<em>Stack：</em><br/>
-						<pre>
-							%s
-						</pre>
+						<pre><em>Stack：</em><br/>%s</pre>
+					</p>
+					<p class="contact">
+						<p class="contact">Plese contact administrator or [ <a href="/">Go Home</a> ]</p>
 					</p>
 				</div>
 		</body>
