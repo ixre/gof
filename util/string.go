82d8a50ae18b5d694b0603038d1b91172878797a
jarryliu <jarrysix@gmail.com>
2016-03-20 12:15:11 +0800

    rand int

--- util/string.go
+++ util/string.go
@@ -10,6 +10,8 @@ package util
 
 import (
 	"crypto/rand"
+	mr "math/rand"
+	"math"
 )
 
 const letterStr = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
@@ -23,4 +25,15 @@ func RandString(n int) string {
 		runes[i] = letterStr[b%byte(lsLen)]
 	}
 	return string(runes)
+}
+
+// 随机整数
+func RandInt(n int)int{
+	min := int(math.Pow10(n-1))
+	max := min * 10 -1
+	v := mr.Intn(max)
+	if v < min{
+		return min + v
+	}
+	return v
 }
