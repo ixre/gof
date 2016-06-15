9f1973af47b208b47d9cb0d056b08f4c478146e6
jarryliu <jarrysix@gmail.com>
2016-06-15 18:13:05 +0800

    template

--- template.go
+++ template.go
@@ -26,30 +26,34 @@ var (
 )
 
 type CachedTemplate struct {
-	basePath      string
-	shareFiles    []string
-	set           map[string]*template.Template
-	mux           *sync.RWMutex
-	winPathRegexp *regexp.Regexp
+	_basePath      string
+	_shareFiles    []string
+	_fsNotify      bool
+	_set           map[string]*template.Template
+	_mux           *sync.RWMutex
+	_winPathRegexp *regexp.Regexp
 }
 
-func NewCachedTemplate(basePath string, files ...string) *CachedTemplate {
+// when notify is false , will not compile on file change!
+func NewCachedTemplate(basePath string,notify bool,files ...string) *CachedTemplate {
 	g := &CachedTemplate{
-		basePath:   basePath,
-		set:        make(map[string]*template.Template, 0),
-		mux:        &sync.RWMutex{},
-		shareFiles: files,
+		_basePath:   basePath,
+		_set:        make(map[string]*template.Template, 0),
+		_mux:        &sync.RWMutex{},
+		_shareFiles: files,
 	}
 	return g.init()
 }
 
 func (this *CachedTemplate) init() *CachedTemplate {
-	for i, v := range this.shareFiles {
-		if !strings.HasPrefix(v, this.basePath) {
-			this.shareFiles[i] = this.basePath + v
+	for i, v := range this._shareFiles {
+		if !strings.HasPrefix(v, this._basePath) {
+			this._shareFiles[i] = this._basePath + v
 		}
 	}
-	go this.fsNotify()
+	if this._fsNotify {
+		go this.fsNotify()
+	}
 	return this
 }
 
@@ -57,17 +61,17 @@ func (this *CachedTemplate) init() *CachedTemplate {
 func (this *CachedTemplate) fileChanged(event *fsnotify.Event) {
 	eventStr := event.String()
 	if runtime.GOOS == "windows" {
-		if this.winPathRegexp == nil {
-			this.winPathRegexp = regexp.MustCompile("\\\\+")
+		if this._winPathRegexp == nil {
+			this._winPathRegexp = regexp.MustCompile("\\\\+")
 		}
-		eventStr = this.winPathRegexp.ReplaceAllString(eventStr, "/")
+		eventStr = this._winPathRegexp.ReplaceAllString(eventStr, "/")
 	}
 	if eventRegexp.MatchString(eventStr) {
 		matches := eventRegexp.FindAllStringSubmatch(eventStr, 1)
 		if len(matches) > 0 {
 			filePath := matches[0][1]
-			if i := strings.Index(filePath, this.basePath); i != -1 {
-				file := filePath[i+len(this.basePath):]
+			if i := strings.Index(filePath, this._basePath); i != -1 {
+				file := filePath[i+len(this._basePath):]
 				if strings.Index(file, "_old_") == -1 &&
 					strings.Index(file, "_tmp_") == -1 &&
 					strings.Index(file, "_swp_") == -1 {
@@ -100,7 +104,7 @@ func (this *CachedTemplate) fsNotify() {
 			}
 		}(this)
 
-		err = filepath.Walk(this.basePath, func(path string,
+		err = filepath.Walk(this._basePath, func(path string,
 			info os.FileInfo, err error) error {
 			if err == nil && info.IsDir() &&
 				info.Name()[0] != '.' { // not hidden file
@@ -120,11 +124,11 @@ func (this *CachedTemplate) fsNotify() {
 
 func (this *CachedTemplate) parseTemplate(name string) (
 	*template.Template, error) {
-	this.mux.Lock() //对写加锁
-	files := append([]string{this.basePath + name},
-		this.shareFiles...) //name需要第一个位置
+	this._mux.Lock() //对写加锁
+	files := append([]string{this._basePath + name},
+		this._shareFiles...) //name需要第一个位置
 	tpl, err := template.ParseFiles(files...)
-	this.mux.Unlock()
+	this._mux.Unlock()
 	return tpl, err
 }
 
@@ -132,7 +136,7 @@ func (this *CachedTemplate) compileTemplate(name string) (
 	*template.Template, error) {
 	tpl, err := this.parseTemplate(name)
 	if err == nil {
-		this.set[name] = tpl
+		this._set[name] = tpl
 		log.Println("[ Gof][ Template][ Compile]: ", name)
 	} else {
 		log.Println("[ Gof][ Template][ Error] -", err.Error())
@@ -142,17 +146,17 @@ func (this *CachedTemplate) compileTemplate(name string) (
 
 func (this *CachedTemplate) Execute(w io.Writer,
 	name string, data interface{}) error {
-	this.mux.RLock() //仅对读加锁
-	tpl, ok := this.set[name]
+	this._mux.RLock() //仅对读加锁
+	tpl, ok := this._set[name]
 	if !ok {
-		this.mux.RUnlock()
+		this._mux.RUnlock()
 		var err error
 		if tpl, err = this.compileTemplate(name); err != nil {
 			return err
 		}
-		this.set[name] = tpl
+		this._set[name] = tpl
 	} else {
-		defer this.mux.RUnlock()
+		defer this._mux.RUnlock()
 	}
 	return tpl.Execute(w, data)
 }
