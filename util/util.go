8932218f4ad4b9675652be037a4fd0ce90422ef0
jarryliu <jarrysix@gmail.com>
2016-09-07 17:12:02 +0800

    encoder

---
+++ util/util.go
@@ -0,0 +1,61 @@
+/**
+ * Copyright 2015 @ z3q.net.
+ * name : string.go
+ * author : jarryliu
+ * date : -- :
+ * description :
+ * history :
+ */
+package util
+
+import (
+    "crypto/rand"
+    "math"
+    mr "math/rand"
+    "golang.org/x/text/encoding/simplifiedchinese"
+    "golang.org/x/text/encoding/traditionalchinese"
+    "golang.org/x/text/encoding"
+)
+
+const letterStr = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+
+//随机字符号串
+func RandString(n int) string {
+    lsLen := len(letterStr)
+    var runes = make([]byte, n)
+    rand.Read(runes)
+    for i, b := range runes {
+        runes[i] = letterStr[b % byte(lsLen)]
+    }
+    return string(runes)
+}
+
+// 随机整数
+func RandInt(n int) int {
+    min := int(math.Pow10(n - 1))
+    max := min * 10 - 1
+    v := mr.Intn(max)
+    if v < min {
+        return min + v
+    }
+    return v
+}
+
+
+//编码
+func EncodingTransform(src []byte, enc string) ([]byte, error) {
+    var ec encoding.Encoding
+    switch enc {
+    default:
+        return src, nil
+    case "GBK":
+        ec = simplifiedchinese.GBK
+    case "GB2312":
+        ec = simplifiedchinese.HZGB2312
+    case "BIG5":
+        ec = traditionalchinese.Big5
+    }
+    dst := make([]byte, len(src) * 2)
+    n, _, err := ec.NewEncoder().Transform(dst, src, true)
+    return dst[:n], err
+}
