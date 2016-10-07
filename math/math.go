83a52f3c6e5e302cab4c43f7085b64b4d64981ab
jarryliu <jarrysix@gmail.com>
2016-10-07 23:41:53 +0800

    math

--- math/math.go
+++ math/math.go
@@ -4,27 +4,28 @@ import (
 	"math"
 )
 
-// 四舍五入计算,通过设置pos来指定要精度位数
-// 如果小于０，则四舍五入到整数为
-func Round(val float64, pos int) float64 {
-	if pos <= 0 {
+// 四舍五入计算,并保留n位精度位
+// 如果n小于０，则四舍五入到整数位
+func Round(val float64, n int) float64 {
+	if n <= 0 {
 		if val < 0 {
 			return math.Ceil(val - 0.5)
 		}
 		return math.Floor(val + 0.5)
 	}
-	digit := math.Pow10(pos)
+	digit := math.Pow10(n)
 	if val < 0 {
 		return math.Ceil(val*digit-0.5) / digit
 	}
 	return math.Floor(val*digit+0.5) / digit
 }
 
-func Round32(val float32, pos int) float32 {
-	return float32(Round(float64(val), pos))
+// 四舍五入计算,并保留n位精度位
+func Round32(val float32, n int) float32 {
+	return float32(Round(float64(val), n))
 }
 
 // 普通近似值计算, 不四舍五入,n为小数点精度
-func FixFloat32(v float64, n int) float64 {
+func FixedFloat(v float64, n int) float64 {
 	return math.Floor(v*math.Pow10(n)) / math.Pow10(n)
 }
