753a2cf326e656fc088fdc7f14e66cb712a50e4d
jarryliu <jarrysix@gmail.com>
2017-11-22 22:43:19 +0800

    util/util.go

--- util/util.go
+++ util/util.go
@@ -3,12 +3,42 @@ package util
 import (
 	"strconv"
 	"strings"
+	"time"
+	"math"
+	"math/rand"
 )
+
+
+const letterStr = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+
+//随机字符号串
+func RandString(n int) string {
+	lsLen := len(letterStr)
+	var runes = make([]byte, n)
+	rand.Read(runes)
+	for i, b := range runes {
+		runes[i] = letterStr[b%byte(lsLen)]
+	}
+	return string(runes)
+}
+
+// 随机整数
+func RandInt(n int) int {
+	min := int(math.Pow10(n - 1))
+	max := min*10 - 1
+	mr.Seed(time.Now().Unix())
+	v := mr.Intn(max)
+	if v < min {
+		return min + v
+	}
+	return v
+}
 
 var (
 	BoolExt *boolExtend   = &boolExtend{}
 	StrExt  *stringExtend = &stringExtend{}
 )
+
 
 type (
 	boolExtend struct {
