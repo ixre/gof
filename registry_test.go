9129033cbddd8ea448278653c3a2c065c455dfc5
jarryliu <jarrysix@gmail.com>
2017-12-09 11:28:36 +0800

    registry_test.go

--- registry_test.go
+++ registry_test.go
@@ -3,9 +3,9 @@ package gof
 import "testing"
 
 func TestRegistry_Set(t *testing.T) {
-	r := NewRegistry("./tmp/conf/", ":")
+	r,err := NewRegistry("./tmp/conf/", ":")
 	key := "core:config:user_name"
-	err := r.Set(key, "jarrysix")
+	err = r.Set(key, "jarrysix")
 	if err != nil {
 		t.Error(err)
 		t.FailNow()
