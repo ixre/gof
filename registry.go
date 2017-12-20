cc42f0cd4afa6da3f0f82a4c098aa4b5fcddb61f
jarryliu <jarrysix@gmail.com>
2017-12-20 21:42:01 +0800

    registry

--- registry.go
+++ registry.go
@@ -12,8 +12,15 @@ import (
 type Registry struct {
 	path    string
 	delimer string
-	data    map[string]*toml.Tree
-	pathMap map[string]string
+	data    map[string]*RegistryTree
+}
+
+type RegistryTree struct {
+	key      string
+	filePath string
+	isRoot   bool
+	tree     *toml.Tree
+	glob     *Registry
 }
 
 // create new registry manager
@@ -29,25 +36,45 @@ func NewRegistry(path string, delimer string) (*Registry, error) {
 
 // load from config files
 func (r *Registry) init() (*Registry, error) {
-	var err error
-	r.data = map[string]*toml.Tree{}
-	r.pathMap = map[string]string{}
-	err = filepath.Walk(r.path, func(path string, info os.FileInfo, err error) error {
-		if err == nil && !info.IsDir() {
-			name := info.Name()
-			if strings.HasSuffix(name, ".conf") {
-				t, err := toml.LoadFile(path)
-				if err == nil {
-					key := name[:len(name)-5]
-					r.data[key] = t
-					r.pathMap[key] = path
-				}
-				return err
-			}
-		}
+	r.data = map[string]*RegistryTree{}
+	return r.dirInit()
+}
+
+func (r *Registry) dirInit() (*Registry, error) {
+	d, err := os.Open(r.path)
+	if err != nil {
+		return r, err
+	}
+	files, err := d.Readdir(-1)
+	if err != nil {
+		return r, err
+	}
+	for _, f := range files {
+		r.load(r.path+"/"+f.Name(), f)
+	}
+	return r, nil
+}
+
+func (r *Registry) load(path string, info os.FileInfo) error {
+	if info.IsDir() {
 		return nil
-	})
-	return r, err
+	}
+	if name := info.Name(); strings.HasSuffix(name, ".conf") {
+		t, err := toml.LoadFile(path)
+		if err == nil {
+			key := name[:len(name)-5]
+			tree := &RegistryTree{
+				isRoot:   true,
+				key:      key,
+				filePath: path,
+				tree:     t,
+				glob:     r,
+			}
+			r.data[key] = tree
+		}
+		return err
+	}
+	return nil
 }
 
 // get file key and config key
@@ -61,71 +88,127 @@ func (r *Registry) split(key string) []string {
 
 // get registry pair
 func (r *Registry) Get(key string) interface{} {
-	arr := r.split(key)
-	fk, tk := arr[0], arr[1]
-	d, exist := r.data[fk]
-	if !exist {
-		panic(errors.New("no such registry " + fk))
-	}
-	if len(arr) == 2 {
-		return d.Get(tk)
-	}
-	tree, result := d.Get(tk).(*toml.Tree)
-	if !result || tree == nil {
-		panic(errors.New("no such node " + tk))
-	}
-	return tree.Get(arr[2])
+	i := strings.Index(key, r.delimer)
+	return r.Use(key[:i]).Get(key[i+1:])
 }
 
-// set registry pair
-func (r *Registry) Set(key string, value interface{}) (err error) {
-	arr := r.split(key)
-	fk, tk := arr[0], arr[1]
-	d, exist := r.data[fk]
-	if !exist {
-		d, err = r.createNode(arr, value)
-		r.data[fk] = d
-		r.pathMap[fk] = r.path + fk + ".conf"
-	} else {
-		if len(arr) == 2 {
-			d.Set(tk, value)
-		} else {
-			tree := d.Get(tk).(*toml.Tree)
-			if tree == nil {
-				return errors.New("no such node " + tk)
-			}
-			tree.Set(arr[2], value)
-		}
-	}
-	_, err = os.Stat(r.path)
-	if os.IsNotExist(err) {
-		err = os.MkdirAll(r.path, os.ModePerm)
-	}
+func (r *Registry) Set(key string, v interface{}) error {
+	i := strings.Index(key, r.delimer)
+	rt := r.Use(key[:i])
+	err := rt.Set(key[i+1:], v)
 	if err == nil {
-		var fi *os.File
-		fi, err = os.OpenFile(r.pathMap[fk], os.O_CREATE|os.O_WRONLY, os.ModePerm)
-		if err == nil {
-			_, err = d.WriteTo(fi)
-			fi.Close()
-		}
+		err = rt.Flush()
 	}
 	return err
+}
+
+func (r *Registry) Use(key string) *RegistryTree {
+	t, b := r.data[key]
+	if !b {
+		t = &RegistryTree{
+			isRoot:   true,
+			key:      key,
+			filePath: r.path + key + ".conf",
+			tree:     nil,
+			glob:     r,
+		}
+	}
+	return t
 }
 
 // get string
 func (r *Registry) GetString(key string) string {
 	return r.Get(key).(string)
 }
+
 func (r *Registry) createNode(arr []string, value interface{}) (*toml.Tree, error) {
-	tk := arr[1]
+	key := arr[0]
 	tree, err := toml.TreeFromMap(map[string]interface{}{})
-	if len(arr) == 2 {
-		tree.Set(tk, value)
+	if len(arr) == 1 {
+		tree.Set(key, value)
 	} else {
 		tree2, _ := toml.TreeFromMap(map[string]interface{}{
-			arr[2]: value,
+			arr[1]: value,
 		})
-		tree.Set(tk, tree2)
+		tree.Set(key, tree2)
 	}
 	return tree, err
+}
+
+// get registry pair
+func (r *RegistryTree) Get(prop string) interface{} {
+	if r.tree != nil {
+		arr := r.desSplit(prop)
+		if len(arr) == 1 {
+			return r.tree.Get(arr[0])
+		}
+		tree, result := r.tree.Get(arr[0]).(*toml.Tree)
+		if !result || tree == nil {
+			return nil
+		}
+		return tree.Get(arr[1])
+	}
+	return nil
+}
+
+func (r *RegistryTree) GetString(prop string) string {
+	return r.Get(prop).(string)
+}
+
+func (r *RegistryTree) GetInt(prop string) int {
+	return r.Get(prop).(int)
+}
+
+func (r *RegistryTree) GetBool(prop string) bool {
+	return r.Get(prop).(bool)
+}
+
+// get file key and config key
+func (r *RegistryTree) desSplit(key string) []string {
+	i := strings.LastIndex(key, r.glob.delimer)
+	if i > 0 {
+		return []string{key[:i], key[i+1:]}
+	}
+	return []string{key}
+}
+
+// set registry pair
+func (r *RegistryTree) Set(prop string, value interface{}) error {
+	arr := r.desSplit(prop)
+	if r.tree == nil {
+		tree, err := r.glob.createNode(arr, value)
+		if err != nil {
+			return err
+		}
+		r.tree = tree
+		r.glob.data[r.key] = r
+		return nil
+	}
+	if len(arr) == 1 {
+		r.tree.Set(arr[0], value)
+	} else {
+		tree := r.tree.Get(arr[0]).(*toml.Tree)
+		if tree == nil {
+			return errors.New("no such node " + arr[0])
+		}
+		tree.Set(arr[1], value)
+	}
+	return nil
+}
+
+func (r *RegistryTree) Flush() error {
+	dir := filepath.Dir(r.filePath)
+	_, err := os.Stat(dir)
+	if os.IsNotExist(err) {
+		err = os.MkdirAll(dir, os.ModePerm)
+	}
+	if err == nil {
+		var fi *os.File
+		fi, err = os.OpenFile(r.filePath, os.O_CREATE|os.O_WRONLY, os.ModePerm)
+		if err == nil {
+			_, err = r.tree.WriteTo(fi)
+			fi.Close()
+		}
+	}
+	return err
 }
--- registry_test.go
+++ registry_test.go
@@ -1,15 +1,29 @@
 package gof
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestRegistry_Set(t *testing.T) {
-	r, err := NewRegistry("./tmp/conf/", ":")
-	key := "core:config:user_name"
-	err = r.Set(key, "jarrysix")
-	if err != nil {
-		t.Error(err)
-		t.FailNow()
+	r, _ := NewRegistry("./tmp/conf/", ".")
+	rt := r.Use("core")
+	key := "config.user_name"
+	val := rt.Get(key)
+	if val == nil {
+		err := rt.Set(key, "jarrysix")
+		if err != nil {
+			t.Error(err)
+			t.FailNow()
+		}
+		rt.Flush()
+		val = rt.Get(key)
 	}
+	t.Log("result :", val.(string))
+}
+
+func TestRegistryOld(t *testing.T) {
+	r, _ := NewRegistry("./tmp/conf/", ".")
+	key := "core.config.user_name"
 	val := r.Get(key)
 	t.Log("result :", val.(string))
 }
