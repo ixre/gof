945caa8ba9ca16e1fed18375d70a0c761ad78be8
jarryliu <jarrysix@gmail.com>
2019-07-12 11:35:50 +0800

    unix_crypto

--- crypto/crypto_test.go
+++ crypto/crypto_test.go
@@ -16,11 +16,17 @@ import (
 
 func TestEncode(t *testing.T){
 	c := NewUnixCrypto("689f8ca2f9d827bf52e1ad0c10b271dc91aea763","")
+	//c := NewUnixCrypto("3ba99c37b552265ea3cec72346511b4c6809f5d0","")
 	r:=c.Encode()
 	s:= string(r)
 	t.Log("encode:",string(s))
 	ds,unix,err := c.Decode([]byte(s))
 	t.Log("--dst:",string(ds),"unix:",unix,err)
+
+	c2 := NewUnixCrypto("3ba99c37b552265ea3cec72346511b4c6809f5d0","")
+	r2:=c2.Encode()
+	s2:= string(r2)
+	t.Log("encode:",string(s2))
 }
 
 func TestDecode(t *testing.T){
