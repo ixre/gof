5f02ce026d410cfddc8bc5170c5b916f3251be77
jarrysix <jarrysix@gmail.com>
2021-03-12 11:12:29 +0800

    fix: RowsToMarshalMap scan interface{} instead of byte[]

--- db/parser.go
+++ db/parser.go
@@ -13,6 +13,32 @@ func RowsToMarshalMap(rows *sql.Rows) (rowsMap []map[string]interface{}) {
 
 	//定义数组，数组的类型为[]byte
 	var values = make([]interface{}, len(columns))
+	var rawBytes = make([]interface{}, len(values))
+	for v := range values {
+		values[v] = &rawBytes[v]
+	}
+	for rows.Next() {
+		rows.Scan(values...)
+		if len(rowsMap) == tmpInt {
+			rowsMap = append(rowsMap, make(map[string]interface{}))
+		}
+		for i, v := range columns {
+			rowsMap[tmpInt][v] = rawBytes[i]
+		}
+		tmpInt++
+	}
+	return rowsMap
+}
+
+//转换为字典数组
+//参考：http://my.oschina.net/nowayout/blog/143278
+func RowsToMarshalMapV1(rows *sql.Rows) (rowsMap []map[string]interface{}) {
+	rowsMap = []map[string]interface{}{} //数据切片
+	var tmpInt = 0                       //序列
+	columns, _ := rows.Columns()         //列名
+
+	//定义数组，数组的类型为[]byte
+	var values = make([]interface{}, len(columns))
 	var rawBytes = make([][]byte, len(values))
 
 	for v := range values {
