7e959ccc0f96c14ce3bfa90f746259cee60b1d9f
jarrysix <jarrysix@gmail.com>
2023-12-11 23:12:21 +0800

    Update tree.go

--- util/tree/tree.go
+++ util/tree/tree.go
@@ -3,11 +3,11 @@ package tree
 // 节点数据
 type NodeData struct {
 	// 子节点编号
-	Id int `json:"-"`
+	Id string `json:"-"`
 	// 文本
-	Name string `json:"name"`
+	Label string `json:"name"`
 	// 父节点编号
-	Parent int `json:"-"`
+	Parent string `json:"-"`
 	// 是否叶子节点
 	IsLeaf bool `json:"expand"`
 	// 其他数据
@@ -16,23 +16,24 @@ type NodeData struct {
 
 // 树形节点
 type TreeNode struct {
-	id int `json:"id"`
+	// 标识
+	id string `json:"id"`
 	// 文本
-	Name string `json:"name"`
+	Label string `json:"name"`
 	// 延迟加载
-	Lazy bool `json:"lazy"`
+	//Lazy bool `json:"lazy"`
 	// 是否叶子节点
 	IsLeaf bool `json:"isLeaf"`
-	// 其他数据
-	Data interface{} `json:"data"`
 	// 子节点
 	Children []*TreeNode `json:"children"`
+	// 其他数据
+	Data interface{} `json:"data"`
 }
 
 func (f NodeData) Node() *TreeNode {
 	return &TreeNode{
 		id:       f.Id,
-		Name:     f.Name,
+		Label:     f.Label,
 		IsLeaf:   f.IsLeaf,
 		Data:     f.Data,
 		Children: []*TreeNode{},
@@ -42,7 +43,7 @@ func (f NodeData) Node() *TreeNode {
 // 转换为树形
 func ParseTree(nodeList []NodeData, nodeFn func(node *TreeNode)) (rootNode *TreeNode) {
 	for i, k := range nodeList {
-		if k.Id == 0 {
+		if k.Id == "" {
 			rootNode = k.Node()
 			nodeList = append(nodeList[:i], nodeList[i+1:]...)
 			break
@@ -50,8 +51,8 @@ func ParseTree(nodeList []NodeData, nodeFn func(node *TreeNode)) (rootNode *Tree
 	}
 	if rootNode == nil {
 		rootNode = &TreeNode{
-			id:       0,
-			Name:     "根节点",
+			id:       "",
+			Label:     "根节点",
 			Children: nil}
 	}
 	walkTree(rootNode, nodeList, nodeFn)
