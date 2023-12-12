913dae021471f53a47024988559fe6d294eafd3f
jarrysix <jarrysix@gmail.com>
2023-12-12 16:08:36 +0800

    Update tree.go

--- util/tree/tree.go
+++ util/tree/tree.go
@@ -3,13 +3,13 @@ package tree
 // 节点数据
 type NodeData struct {
 	// 子节点编号
-	Id string `json:"-"`
+	Id string `json:"id"`
 	// 文本
-	Label string `json:"name"`
+	Label string `json:"label"`
 	// 父节点编号
-	Parent string `json:"-"`
+	ParentId string `json:"parentId"`
 	// 是否叶子节点
-	IsLeaf bool `json:"expand"`
+	IsLeaf bool `json:"isLeaf"`
 	// 其他数据
 	Data interface{} `json:"data"`
 }
@@ -17,11 +17,9 @@ type NodeData struct {
 // 树形节点
 type TreeNode struct {
 	// 标识
-	id string `json:"id"`
+	Id string `json:"id"`
 	// 文本
-	Label string `json:"name"`
-	// 延迟加载
-	//Lazy bool `json:"lazy"`
+	Label string `json:"label"`
 	// 是否叶子节点
 	IsLeaf bool `json:"isLeaf"`
 	// 子节点
@@ -32,8 +30,8 @@ type TreeNode struct {
 
 func (f NodeData) Node() *TreeNode {
 	return &TreeNode{
-		id:       f.Id,
-		Label:     f.Label,
+		Id:       f.Id,
+		Label:    f.Label,
 		IsLeaf:   f.IsLeaf,
 		Data:     f.Data,
 		Children: []*TreeNode{},
@@ -41,28 +39,23 @@ func (f NodeData) Node() *TreeNode {
 }
 
 // 转换为树形
-func ParseTree(nodeList []NodeData, nodeFn func(node *TreeNode)) (rootNode *TreeNode) {
+func ParseTree(nodeList []NodeData, nodeFn func(node *TreeNode)) []*TreeNode {
+	root := &TreeNode{}
 	for i, k := range nodeList {
 		if k.Id == "" {
-			rootNode = k.Node()
+			root = k.Node()
 			nodeList = append(nodeList[:i], nodeList[i+1:]...)
 			break
 		}
 	}
-	if rootNode == nil {
-		rootNode = &TreeNode{
-			id:       "",
-			Label:     "根节点",
-			Children: nil}
-	}
-	walkTree(rootNode, nodeList, nodeFn)
-	return rootNode
+	walkTree(root, nodeList, nodeFn)
+	return root.Children
 }
 
 func walkTree(node *TreeNode, nodeList []NodeData, nodeFn func(node *TreeNode)) {
 	node.Children = []*TreeNode{}
 	for _, v := range nodeList {
-		if v.Parent == node.id {
+		if v.ParentId == node.Id {
 			n := v.Node()
 			if nodeFn != nil {
 				nodeFn(n)
